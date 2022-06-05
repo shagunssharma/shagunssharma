b7909b45b4fb6003fb52cb2cc2249a20947f2b8c
Shagun Sharma <92916869+shagunssharma@users.noreply.github.com>
2022-06-05 13:15:33 +0530

    JS Loops

---
+++ JS Loops/app.js
@@ -0,0 +1,6 @@
+let message = paseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(promt("Enter a valid number!"));
+}
+const targetNum = Math.florr((Math.random() * maximum)) + 1;
+
